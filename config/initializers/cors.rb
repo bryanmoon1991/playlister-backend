dec189fc1ec69e8d35169feb14b25447fe741980
bryanmoon1991 <bryanmoon1991@gmail.com>
2021-03-03 17:49:28 -0500

    updated cors setting

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,8 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # origins '*'
-    # deployed cors
-    origins 'https://playlister-frontend.herokuapp.com/'
+    origins '*', 'https://playlister-frontend.herokuapp.com/'
 
     resource '*',
       headers: :any,
